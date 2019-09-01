4715814b6355bb37d2cfcd2c88a7eabb9bbf3b26
Eric Douglas <eric.douglas.mail@gmail.com>
2019-09-01 07:16:39 -0300

    Updates

---
+++ precache-manifest.b6d90cc7fdb2332c2551aeec4fb1d938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75c85c063342fc5a41caca2dd07c1d2",
+    "url": "/onli-reducer-example/index.html"
+  },
+  {
+    "revision": "a95c9fdb8bd86e56f668",
+    "url": "/onli-reducer-example/static/css/main.b43a3348.chunk.css"
+  },
+  {
+    "revision": "2654b80537089df3a560",
+    "url": "/onli-reducer-example/static/js/2.514818d0.chunk.js"
+  },
+  {
+    "revision": "a95c9fdb8bd86e56f668",
+    "url": "/onli-reducer-example/static/js/main.8b2e1297.chunk.js"
+  },
+  {
+    "revision": "a39e194b437841df09fb",
+    "url": "/onli-reducer-example/static/js/runtime~main.511a3ece.js"
+  }
+]);
